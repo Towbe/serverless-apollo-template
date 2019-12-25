9628a29318e6faf18ce75ec48717a67c182ce5ff
Ali Al Amine <ali@towbe.com>
2019-12-25 22:32:35 +0200

    Fixing the upstream ref

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -10,6 +10,8 @@ readline.question('What is the git url of the new node? ', (url) => {
     console.log('Initializing origin');
     exec('git remote rm origin');
     exec('git remote add origin ' + url);
+    exec('git branch --set-upstream-to origin/master');
     console.log('Pushing the first version');
     exec('git push');
+    console.log('First version pushed');
 });
