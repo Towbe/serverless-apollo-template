dee8eb443615c3196b6ea1238902f62a99e39812
Ali Al Amine <ali@towbe.com>
2020-05-02 17:18:18 +0300

    Add the JSON scalar type

--- src/gql/resolvers.js
+++ src/gql/resolvers.js
@@ -1,9 +1,11 @@
 import queries from './queries';
 import mutations from './mutations';
+import GraphQLJSON from 'graphql-type-json';
 
 const resolvers = {
   Query: queries,
   Mutation: mutations,
+  JSON: GraphQLJSON
 };
 
 export default resolvers;
